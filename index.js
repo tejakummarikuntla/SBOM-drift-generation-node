8a0acd5f9e9f294daac6c5fc3c550f435543ddbd
Teja Kummarikuntla <34749692+tejakummarikuntla@users.noreply.github.com>
2024-01-31 13:58:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 var express = require('express');
+var axios = require('axios'); // Import axios
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello!" }');
+    res.send('{ "response": "Hello From Thetips4you" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Willing!!!" }');
+    res.send('{ "response": "Hello World" }');
 });
+
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "Great!, It works!" }');
 });
+
+// New endpoint using axios to fetch data from an external API
+app.get('/api/users', function (req, res) {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+         .then(function (response) {
+             // Handle success
+             res.send(response.data);
+         })
+         .catch(function (error) {
+             // Handle error
+             res.status(500).send({ error: "Error fetching data" });
+         });
+});
+
 app.listen(process.env.PORT || 3000);
 module.exports = app;
